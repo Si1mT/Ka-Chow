ad1bdb3eeacda7e781f32ddb2ac6def95354ba23
Si1mT <33309403+Si1mT@users.noreply.github.com>
2017-11-09 00:03:14 +0200

    read text file

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("mis teha tahate(create/edit/delete)");
+            //string ans = Console.ReadLine();
+            //if (ans == create)
+            //{
+
+            //}
+            //Console.ReadLine();
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("C:\\Sample.txt");
+
+                //Read the first line of text
+                line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the lie to console window
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
