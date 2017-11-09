a3d0cff1a9ef1223265b8af3671dcbb42bb1c794
Si1mT <33309403+Si1mT@users.noreply.github.com>
2017-11-09 08:51:31 +0200

    choise

--- Program.cs
+++ Program.cs
@@ -19,36 +19,46 @@ namespace ConsoleApp7
             //}
             //Console.ReadLine();
             String line;
-            try
+            Console.WriteLine("kas tahad näha märkmet?(y/n)");
+            string ans = Console.ReadLine();
+            if (ans =="y")
             {
-                //Pass the file path and file name to the StreamReader constructor
-                StreamReader sr = new StreamReader("C:\\Users\\opilane\\Desktop\\notes\\geodwad.txt");
-
-                //Read the first line of text
-                line = sr.ReadLine();
-
-                //Continue to read until you reach end of file
-                while (line != null)
+                try
                 {
-                    //write the lie to console window
-                    Console.WriteLine(line);
-                    //Read the next line
-                    line = sr.ReadLine();
-                }
+                    //Pass the file path and file name to the StreamReader constructor
+                    StreamReader sr = new StreamReader("C:\\Users\\opilane\\Desktop\\notes\\geodwad.txt");
 
-                //close the file
-                sr.Close();
+                    //Read the first line of text
+                    line = sr.ReadLine();
+
+                    //Continue to read until you reach end of file
+                    while (line != null)
+                    {
+                        //write the lie to console window
+                        Console.WriteLine(line);
+                        //Read the next line
+                        line = sr.ReadLine();
+                    }
+
+                    //close the file
+                    sr.Close();
+                    Console.ReadLine();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Executing finally block.");
+                }
+                Console.ReadKey();
+            }
+            else if (ans =="n")
+            {
+                Console.WriteLine("ok");
                 Console.ReadLine();
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Executing finally block.");
-            }
-            Console.ReadKey();
         }
     }
 }
