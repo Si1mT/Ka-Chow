54e917db643a3e96179fecbac7098b485d0ad573
Si1mT <33309403+Si1mT@users.noreply.github.com>
2017-11-09 09:30:51 +0200

    trying to make delete method

--- Class1.cs
+++ Class1.cs
@@ -7,21 +7,13 @@ using System.IO;
 
 namespace ConsoleApp7
 {
-    //class Class1
-    //{ String line;
-    //    ***try***  
-    //{
-    //    StreamReader sr = new StreamReader("C:\Users\SIIM\Desktop\notes.txt");
-    //    line = sr.ReadLine();
-    //        while (line!= null){
-    //        Console.WriteLine(line);
-    //        line = sr.ReadLine();
-    //        }
-    //sr.Close();
-    //    Console.ReadLine();}
-    //   catch(Expection e){
-    //Console.WriteLine("Exception: "+e.Message);
-    //}
-    //finally{
-    //Console.WriteLine("executing final block);
+    class Delete
+    {
+        static void delete()
+        {
+        string f=Get.Files=Path("C:\\Users\\opilane\\Desktop\\notes\\geodwad - Copy (2)");
+            File.Delete(f); 
+
+        }
+    }
 }
